a65a485409b2dd99bde9c6a9146f542281b6ab7a
Strider <strange.transistor@gmail.com>
2018-11-19 16:52:03 +0300

    Bud: test.

--- test/Bud.test.js
+++ test/Bud.test.js
@@ -1,5 +1,6 @@
 
 import Bud from 'lib/Bud'
+import Nothing from 'lib/Nothing'
 
 describe('Bud', () =>
 {
@@ -10,5 +11,17 @@ describe('Bud', () =>
 		var bud = Bud()
 
 		expect(bud).an('object')
+
+		expect(bud.constructor).eq(Bud)
+
+		expect(bud).property('id')
+		expect(bud.id).a('string')
+		expect(bud.id).match(/^B\/\d+$/)
+
+		expect(bud).property('value')
+		expect(bud.value).eq(Nothing)
+
+		expect(bud.evaluate).a('function')
+		expect(bud.emit).a('function')
 	})
 })
