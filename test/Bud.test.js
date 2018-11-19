a135fd559aedf1bf70b3aaa620a6ddf4aa52e187
Strider <strange.transistor@gmail.com>
2018-11-19 16:55:55 +0300

    Bud: Noop.

--- test/Bud.test.js
+++ test/Bud.test.js
@@ -4,24 +4,34 @@ import Nothing from 'lib/Nothing'
 
 describe('Bud', () =>
 {
-	it('Bud()')
-
-	it('Bud(fn)', () =>
+	it('Bud()', () =>
 	{
 		var bud = Bud()
 
-		expect(bud).an('object')
+		expect_bud(bud)
+	})
 
-		expect(bud.constructor).eq(Bud)
+	it('Bud(fn)', () =>
+	{
+		var bud = Bud(() => 1917)
 
-		expect(bud).property('id')
-		expect(bud.id).a('string')
-		expect(bud.id).match(/^B\/\d+$/)
-
-		expect(bud).property('value')
-		expect(bud.value).eq(Nothing)
-
-		expect(bud.evaluate).a('function')
-		expect(bud.emit).a('function')
+		expect_bud(bud)
 	})
 })
+
+export default function expect_bud (bud)
+{
+	expect(bud).an('object')
+
+	expect(bud.constructor).eq(Bud)
+
+	expect(bud).property('id')
+	expect(bud.id).a('string')
+	expect(bud.id).match(/^B\/\d+$/)
+
+	expect(bud).property('value')
+	expect(bud.value).eq(Nothing)
+
+	expect(bud.evaluate).a('function')
+	expect(bud.emit).a('function')
+}
