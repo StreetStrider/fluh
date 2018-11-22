38e9671666a045d0e06ab15fe28dfb1396f04180
Strider <strange.transistor@gmail.com>
2018-11-22 22:19:56 +0300

    Many: test.

---
+++ test/Many.test.js
@@ -0,0 +1,27 @@
+
+import Many from 'lib/Many'
+
+describe('Many', () =>
+{
+	it('Many()', () =>
+	{
+		var many = Many(1, 2, 3)
+
+		expect(many).an('array')
+		expect(many).deep.eq([ 1, 2, 3 ])
+
+		expect(Many.is(many)).true
+		expect(Array.isArray(many)).true
+	})
+
+	it('Array is not Many', () =>
+	{
+		var fake = [ 1, 2, 3 ]
+
+		expect(fake).an('array')
+		expect(fake).deep.eq([ 1, 2, 3 ])
+
+		expect(Many.is(fake)).false
+		expect(Array.isArray(fake)).true
+	})
+})
