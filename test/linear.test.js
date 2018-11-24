329ee3c8d486370da22ef22e0df22905d71fcbb4
Strider <strange.transistor@gmail.com>
2018-11-25 00:29:40 +0300

    linear: test Many.

--- test/linear.test.js
+++ test/linear.test.js
@@ -2,6 +2,7 @@
 import Bud from 'lib/Bud'
 import join from 'lib/join'
 import Nothing from 'lib/Nothing'
+import Many from 'lib/Many'
 
 import { expect_bud } from './Bud.test'
 
@@ -155,5 +156,63 @@ describe('linear', () =>
 		expect(c.value).eq(400)
 
 		expect(cs.callCount).eq(3)
+	})
+
+	it('A → B(Many) → C', () =>
+	{
+		var a = Bud()
+
+		var b = join(a, a => Many(a, a * 10))
+		var c = join(b, b => b * 100)
+
+		var order = []
+
+		var bs = []
+		b.on(track('b'))
+		b.on(value => bs.push(value))
+
+		var cs = []
+		c.on(track('c'))
+		c.on(value => cs.push(value))
+
+		expect(a.value).eq(Nothing)
+		expect(b.value).eq(Nothing)
+		expect(c.value).eq(Nothing)
+
+		a.emit(5)
+		a.emit(6)
+		a.emit(7)
+
+		expect(a.value).eq(7)
+		expect(b.value).eq(70)
+		expect(c.value).eq(7000)
+
+		expect(bs).deep.eq([ 5, 50, 6, 60, 7, 70 ])
+		expect(cs).deep.eq([ 500, 5000, 600, 6000, 700, 7000 ])
+		expect(order).deep.eq(
+		[
+			[ 'b', 5 ],
+			[ 'c', 500 ],
+			[ 'b', 50 ],
+			[ 'c', 5000 ],
+			[ 'b', 6 ],
+			[ 'c', 600 ],
+			[ 'b', 60 ],
+			[ 'c', 6000 ],
+			[ 'b', 7 ],
+			[ 'c', 700 ],
+			[ 'b', 70 ],
+			[ 'c', 7000 ],
+		])
+
+		function track (name)
+		{
+			return (value) =>
+			{
+				order.push([ name, value ])
+
+				return value
+			}
+		}
 	})
 })
