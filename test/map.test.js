1aef0a532ae49fe7195497ff02eb19674cab0c98
Strider <strange.transistor@gmail.com>
2018-11-25 00:42:42 +0300

    map: test Many.

--- test/map.test.js
+++ test/map.test.js
@@ -1,6 +1,7 @@
 
 import Bud from 'lib/Bud'
 import Nothing from 'lib/Nothing'
+import Many from 'lib/Many'
 
 describe('map', () =>
 {
@@ -58,5 +59,31 @@ describe('map', () =>
 				}
 			}
 		}
+	})
+
+	it('produce Many', () =>
+	{
+		var rs1 = []
+		var rs2 = []
+
+		var a = Bud()
+		var b = a.map(a => Many(a + 'b1', a + 'b2'))
+		var c = b.map(b => Many(b + 'c1', b + 'c2'))
+
+		b.on(v => rs1.push(v))
+		c.on(v => rs2.push(v))
+
+		a
+		.emit(1)
+		.emit(2)
+		.emit(3)
+
+		expect(rs1).deep.eq([ '1b1', '1b2', '2b1', '2b2', '3b1', '3b2' ])
+		expect(rs2).deep.eq(
+		[
+			'1b1c1', '1b2c1', '1b1c2', '1b2c2',
+			'2b1c1', '2b2c1', '2b1c2', '2b2c2',
+			'3b1c1', '3b2c1', '3b1c2', '3b2c2',
+		])
 	})
 })
