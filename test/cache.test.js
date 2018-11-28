94b6adee077c594228caf639839de315aa258888
Strider <strange.transistor@gmail.com>
2018-11-28 05:00:01 +0300

    cache: test consistency.

---
+++ test/cache.test.js
@@ -0,0 +1,55 @@
+
+import Bud from 'lib/Bud'
+import join from 'lib/join'
+
+
+describe('cache', () =>
+{
+	it('works', () =>
+	{
+		var a = Bud()
+
+		expect(a.order).deep.eq([])
+
+		var b = join(a, a => a)
+
+		expect(a.order).deep.eq([ b ])
+		expect(b.order).deep.eq([])
+
+		var bs = spy()
+		b.on(bs)
+
+		a.emit(1)
+		expect(bs.callCount).eq(1)
+
+		var c = join(b, b => b)
+
+		expect(a.order).deep.eq([ b, c ])
+		expect(b.order).deep.eq([ c ])
+		expect(c.order).deep.eq([])
+
+		var cs = spy()
+		c.on(cs)
+
+		a.emit(1)
+
+		expect(bs.callCount).eq(2)
+		expect(cs.callCount).eq(2)
+
+		var d = join(a, c, (a, c) => a + c)
+
+		expect(a.order).deep.eq([ b, c, d ])
+		expect(b.order).deep.eq([ c, d ])
+		expect(c.order).deep.eq([ d ])
+		expect(d.order).deep.eq([])
+
+		var ds = spy()
+		d.on(ds)
+
+		a.emit(1)
+
+		expect(bs.callCount).eq(3)
+		expect(cs.callCount).eq(3)
+		expect(ds.callCount).eq(2)
+	})
+})
