e38b6b8f119c4f783b632f649072a00d74ae2a0b
Strider <strange.transistor@gmail.com>
2018-12-01 22:38:19 +0300

    start: use emit and map.

--- start.js
+++ start.js
@@ -5,16 +5,22 @@ import { log } from './lib/realm'
 
 log.enabled = true
 
-var hello = Bud(() => 'Hello')
+var hello = Bud().emit('Hello')
 var name = Bud()
 
-var name_c = join(name, (name) => name[0].toUpperCase() + name.slice(1))
+var name_c = name.map(name =>
+{
+	return name[0].toUpperCase() + name.slice(1)
+})
 
-var hello_name = join(hello, name_c, (hello, name) => `${ hello }, ${ name }`)
+var hello_name = join(hello, name_c, (hello, name) =>
+{
+	return `${ hello }, ${ name }`
+})
 
-var hello_name_excl = join(hello_name, (s) => s + '!')
+var hello_name_excl = hello_name.map(s => s + '!')
 
 name.emit('world')
-name.emit('world')
+// name.emit('world')
 
 log('*', hello_name_excl.value)
