905323c5c549a5e6b1eb60dfd6019c3886336899
Strider <strange.transistor@gmail.com>
2018-12-02 16:30:04 +0300

    experimenting with streams that can end.

--- start.js
+++ start.js
@@ -1,26 +1,58 @@
 
 import Bud  from './lib/Bud'
 import join from './lib/join'
+import End from './lib/End'
+
 import { log } from './lib/realm'
 
 log.enabled = true
 
 var hello = Bud().emit('Hello')
-var name = Bud()
+var name  = Bud()
 
-var name_c = name.map(name =>
+var name_c = name.map(when_data(name =>
 {
 	return name[0].toUpperCase() + name.slice(1)
-})
+}))
 
-var hello_name = join(hello, name_c, (hello, name) =>
+var hello_name = join(hello, name_c, when_data_all((hello, name) =>
 {
 	return `${ hello }, ${ name }`
-})
+}))
 
-var hello_name_excl = hello_name.map(s => s + '!')
+var hello_name_excl = hello_name.map(when_data(s => s + '!'))
+
+hello_name_excl.on(value => console.log('*', value))
+
 
 name.emit('world')
-// name.emit('world')
+name.emit(End)
 
-log('*', hello_name_excl.value)
+
+function when_data (fn)
+{
+	return (value) =>
+	{
+		if (value !== End)
+		{
+			return fn(value)
+		}
+		else
+		{
+			return value
+		}
+	}
+}
+
+function when_data_all (fn)
+{
+	return (...values) =>
+	{
+		if (values.some(value => value === End))
+		{
+			return End
+		}
+
+		return fn(...values)
+	}
+}
