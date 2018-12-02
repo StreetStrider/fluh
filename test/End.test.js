dfe0a054d4976fac1123577fe75c74f3ca6add74
Strider <strange.transistor@gmail.com>
2018-12-02 16:15:52 +0300

    End: test in the middle of Many.

--- test/End.test.js
+++ test/End.test.js
@@ -2,7 +2,7 @@
 import Bud from 'lib/Bud'
 // import join from 'lib/join'
 // import Nothing from 'lib/Nothing'
-// import Many from 'lib/Many'
+import Many from 'lib/Many'
 import End from 'lib/End'
 
 describe.only('End', () =>
@@ -24,6 +24,30 @@ describe.only('End', () =>
 		a.emit(3)
 		a.emit(End)
 		a.emit(4)
+
+		expect(a.value).eq(End)
+		expect(b.value).eq(End)
+
+		expect(rs).deep.eq([ 1, 2, 3, End ])
+		expect(s.callCount).eq(4)
+	})
+
+	it('End work in midlle of Many', () =>
+	{
+		var a = Bud()
+
+		var b = a.map(v => v)
+
+		var rs = []
+		b.on(value => rs.push(value))
+
+		var s = spy()
+		b.on(s)
+
+		a.emit(1)
+		a.emit(Many(2, 3, End))
+		a.emit(4)
+		a.emit(Many(5, 6))
 
 		expect(a.value).eq(End)
 		expect(b.value).eq(End)
