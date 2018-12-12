33df5f8259f19396147655ff2c259b574002a476
Strider <strange.transistor@gmail.com>
2018-12-12 23:30:10 +0300

    error example.

--- start.js
+++ start.js
@@ -7,39 +7,24 @@ import { when_data } from './map/when'
 
 import { log } from './lib/realm'
 
-log.enabled = true
+// log.enabled = true
 
-var hello = Bud().emit('Hello')
-var name  = Bud()
+var a = Bud()
 
-var name_c = name.map(when_data(name =>
+var b = a.map(v =>
 {
-	return name[0].toUpperCase() + name.slice(1)
-}))
-
-var hello_name = join(hello, name_c, when_data_all((hello, name) =>
-{
-	return `${ hello }, ${ name }`
-}))
-
-var hello_name_excl = hello_name.map(when_data(s => s + '!'))
-
-hello_name_excl.on(value => console.log('*', value))
-
-
-name.emit('world')
-name.emit(End)
-
-
-function when_data_all (fn)
-{
-	return (...values) =>
+	if (v === 0)
 	{
-		if (values.some(value => value === End))
-		{
-			return End
-		}
-
-		return fn(...values)
+		throw new TypeError('zero')
 	}
-}
+
+	return (1 / v)
+})
+
+b.on(v => console.log('v', v))
+
+a
+.emit(1)
+.emit(2)
+.emit(3)
+.emit(0)
