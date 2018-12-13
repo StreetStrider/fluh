487ae3e1157d77f31de7db36c34acbf289b6314c
Strider <strange.transistor@gmail.com>
2018-12-13 23:05:11 +0300

    example capture.

--- start.js
+++ start.js
@@ -3,6 +3,7 @@ import { Bud }  from './'
 import { join } from './'
 import { End }  from './'
 
+import capture from './lib/capture'
 import { when_data } from './map/when'
 
 import { log } from './lib/realm'
@@ -11,7 +12,7 @@ import { log } from './lib/realm'
 
 var a = Bud()
 
-var b = a.map(v =>
+var b = a.map(capture(v =>
 {
 	if (v === 0)
 	{
@@ -19,12 +20,13 @@ var b = a.map(v =>
 	}
 
 	return (1 / v)
-})
+}))
 
 b.on(v => console.log('v', v))
+b.on(when_data(v => console.log('d', v)))
 
 a
+.emit(0)
 .emit(1)
 .emit(2)
 .emit(3)
-.emit(0)
