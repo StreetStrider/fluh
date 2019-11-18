8c1062f7ebfe6ee7861d4ce225c84e1215e31091
Strider <strange.transistor@gmail.com>
2019-11-18 22:26:26 +0300

    not todo.

--- start.js
+++ start.js
@@ -1,17 +1,18 @@
+// TODO: turnoff & cleanup
 
 import { Bud }  from '.'
 import { End }  from '.'
 
 import { when_data } from './map/when'
-import { when_end } from './map/when'
+import { when_end }  from './map/when'
 
 var a = Bud()
+.on(tap('a'))
 
-a.on((x) => console.log('a', x))
+var b = a
+.map(when_data(x => (x + 1)))
+.on(tap('b'))
 
-var b = a.map(when_data(x => x + 1))
-
-b.on((x) => console.log('b', x))
 
 turnoff(b, a)
 
@@ -22,4 +23,9 @@ b.emit(400).emit(End)
 function turnoff (a, b)
 {
 	return a.on(when_end(b.emit))
+}
+
+function tap (label)
+{
+	return (x) => (console.log(label, x), x)
 }
