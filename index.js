7543abe953d6424593443ada17e4967373969719
Strider <strange.transistor@gmail.com>
2019-11-18 22:17:47 +0300

    test index.

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ export { default as Many } from './lib/Many'
 export { default as End } from './lib/End'
 
 export { default as Noop } from './lib/Noop'
+export { default as Same } from './lib/Same'
+export { default as Fin }  from './lib/Fin'
+
+export { default as asap } from './lib/asap'
+export { default as capture } from './lib/capture'
+export { default as concat } from './lib/concat'
+export { default as drain } from './lib/drain'
 
 export { default as join } from './lib/join'
---
+++ test/index.test.js
@@ -0,0 +1,26 @@
+
+import * as index from '../'
+
+
+describe('index', () =>
+{
+	it('exports', () =>
+	{
+		expect(index.Bud).a('function')
+
+		expect(index.Nothing).a('symbol')
+		expect(index.Many).a('function')
+		expect(index.End).a('symbol')
+
+		expect(index.Noop).a('function')
+		expect(index.Same).a('function')
+		expect(index.Fin).a('function')
+
+		expect(index.asap).a('function')
+		expect(index.capture).a('function')
+		expect(index.concat).a('function')
+		expect(index.drain).a('function')
+
+		expect(index.join).a('function')
+	})
+})
