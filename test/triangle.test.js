dbc1fe1c9d0bc1b919d2c0791505abfc2b6ea025
Strider <strange.transistor@gmail.com>
2019-12-04 18:37:34 +0300

    check order in triangle case when end occur.

--- test/triangle.test.js
+++ test/triangle.test.js
@@ -92,23 +92,38 @@ describe('triangle', () =>
 		var s = spy()
 		c.on(s)
 
+		expect(a.order).deep.eq([ b, c ])
+		expect(b.order).deep.eq([ c ])
+		expect(c.order).deep.eq([])
+
 		expect(b.value).eq(1)
 		expect(c.value).eq(1)
 		expect(s.callCount).eq(1)
 
+		/* - */
 		a.emit(2)
 
 		expect(b.value).eq(2)
 		expect(c.value).eq(2)
 		expect(s.callCount).eq(2)
 
+		/* - */
 		a.emit(3)
+
+		expect(a.order).deep.eq([ b, c ])
+		expect(b.order).deep.eq([ c ])
+		expect(c.order).deep.eq([])
 
 		expect(b.value).eq(End)
 		expect(c.value).eq(3)
 		expect(s.callCount).eq(3)
 
+		/* - */
 		a.emit(4)
+
+		expect(a.order).deep.eq([ b, c ])
+		expect(b.order).deep.eq([ c ])
+		expect(c.order).deep.eq([])
 
 		expect(b.value).eq(End)
 		expect(c.value).eq(4)
