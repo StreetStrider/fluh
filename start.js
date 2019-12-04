ba1eadad089aec819dddd3f261a4f87fa76bcb65
Strider <strange.transistor@gmail.com>
2019-12-05 02:01:00 +0300

    experiment with finalization.

--- start.js
+++ start.js
@@ -4,26 +4,38 @@ import { Bud }  from '.'
 import { End }  from '.'
 
 import { when_data } from './map/when'
-import { when_end }  from './map/when'
+// import { when_end }  from './map/when'
 
 var a = Bud()
 .on(tap('a'))
 
-var b = a
-.map(when_data(x => (x + 1)))
+/* var b = */
+a.map(when_data(hundred))
 .on(tap('b'))
 
 
-turnoff(b, a)
+// turnoff(b, a)
 
-a.emit(1).emit(2)
-b.emit(400).emit(End)
+a
+.emit(1)
+.emit(2)
+.emit(3)
+.emit(End)
+
+// b.emit(100).emit(End)
 
 
+function hundred (x)
+{
+	return (x + 100)
+}
+
+/*
 function turnoff (a, b)
 {
 	return a.on(when_end(b.emit))
 }
+*/
 
 function tap (label)
 {
