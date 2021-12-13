c847d509438898bfc786d8882bcf81c659e98518
Strider <strange.transistor@gmail.com>
2021-12-14 02:35:45 +0300

    example: debugging.

---
+++ examples/debugging.js
@@ -0,0 +1,23 @@
+import { Bud } from 'fluh'
+import { join } from 'fluh'
+import { when_data_all } from 'fluh/map/when'
+import { End } from 'fluh'
+
+function identity (x) {
+	return x
+}
+
+const source = Bud()
+const a = source.map(identity)
+const b = source.map(identity)
+const drain = join(a, b, when_data_all((a, b) => [a, b]))
+
+source.debug('source')
+a.debug()
+b.debug()
+drain.debug('drain')
+
+source
+.emit('Foo')
+.emit('Bar')
+.emit(End)
